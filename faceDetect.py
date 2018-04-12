d9fd65e945ac393f3fc12352c4cc88e6286592e7
Natalie Cardinali <NatalievCardinali@gmail.com>
2018-04-12 19:34:20 -0400

    most recent files pulled from pi

---
+++ faceDetect.py
@@ -0,0 +1,41 @@
+import json
+import requests
+import cognitive_face as CF
+
+def setup():
+  key = 'f37f3debd907479cbf0d503f5f059214'  # Replace with a valid Subscription Key here.
+  CF.Key.set(key)
+  base_url = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+  CF.BaseUrl.set(base_url)
+  
+  headers = {'Content-Type': 'application/octet-stream',
+             'Ocp-Apim-Subscription-Key': key}
+  url = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect'
+  params = {
+      'returnFaceId': 'true',
+      'returnFaceAttributes':'emotion',
+      #'returnFaceLandmarks' : 'true',
+}
+
+def auth(face):
+  # Gets the binary file data so we can send it to MCS
+  
+  data = open(face, 'rb')
+  response = requests.post(url, headers=headers, data=data, params=params)
+  
+  data = open('Trevor.jpg', 'rb')
+  response2 = requests.post(url, headers=headers, data=data, params=params)
+  
+  parsed = response.json()
+  #print(parsed)
+  
+  requests.packages.urllib3.disable_warnings()
+  json_data = json.loads(response.text)
+  json_data2 = json.loads(response2.text)
+  # = CF.face_list.create(face_list_id='0120', name='natalie', user_data=json_data2[0]['faceId'])
+  #CF.face_list.add_face(image='natalie.jpg', face_list_id='0120')
+  #group = CF.face_list.get('0120')
+  similarity = CF.face.verify(json_data[0]['faceId'], json_data2[0]['faceId'])
+  print(similarity.get('isIdentical'))
+  print("%" + str(similarity.get('confidence')*100))
+
--- mainGateway.py
+++ mainGateway.py
@@ -3,13 +3,8 @@ import time
 import picamera
 import RPi.GPIO as GPIO
 import requests
-
 import sonar
-
-key = ''  # Replace with a valid Subscription Key here.
-CF.Key.set(key)
-base_url = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
-CF.BaseUrl.set(base_url)
+import faceDetect
 
 #Sets up board for reading in the from the camera
 GPIO.setwarnings(False)
@@ -18,18 +13,6 @@ camera = picamera.PiCamera()
 
 #Sets up paths
 requests.packages.urllib3.disable_warnings()
-face = 'FaceSensor/face'
-count = 1
-params = {
-    'returnFaceId': 'true',
-    'returnFaceAttributes':'emotion',
-    #'returnFaceLandmarks' : 'true',
-}
-headers = {'Content-Type': 'application/octet-stream',
-           'Ocp-Apim-Subscription-Key': key}
-
-url = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect'
-
 
 
 while True:
@@ -53,14 +36,9 @@ while True:
         locFace = locFace + '.jpg'
         print("Intruder detected")
         camera.capture(locFace)
-        time.sleep(10)
-        #data = open(locFace, 'rb')
-        #response = requests.post(url, headers=headers, data=data, params=params)
-        #similarity = CF.face.verify(face, defaultFace)
-        #if (similarity.get('isIdentical')):
-        #    print("Hello Natalie")
-        #else:
-        #    print("Unauthorized Personal")
+        faceDetect.auth(locface)
+        time.sleep(5)
+     
 
 
 img_urls = [
